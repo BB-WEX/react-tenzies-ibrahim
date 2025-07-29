58c91dd79b23edf7bd945bd5ecb96396bec0f01d
Ibrahimm08 <imaqsood360@outlook.com>
2025-07-29 16:57:50 +0100

    Added game logic functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-import { use, useEffect, useState } from "react";
+import { use, useEffect, useId, useState } from "react";
 import Die from "./Die";
+import {nanoid} from "nanoid";
 
 function App() {
   const [value, setValue] = useState(0);
   const [isHeld, setIsHeld] = useState(false);
+  const [dice, setDice] = useState(allNewDice());
 
-  const holdDie = () => {
-      setIsHeld(!isHeld);
-  };
+  function holdDie(id) {
+    setDice((beforeDices) =>
+      beforeDices.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  }
+
+  function rollDice() {
+    setDice((beforeDices) =>
+      beforeDices.map((die) => (die.isHeld ? die : generateNewDie()))
+    );
+  }
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
+  function allNewDice() {
+    const dice = [];
+    for (let i = 0; i < 10; i++) {
+      dice.push(generateNewDie());
+    }
+    return dice;
+  }
+
+  const placeDice = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      id={die.id}
+      holdDie={holdDie}
+    />
+  ));
 
   return (
     <div className="board">
@@ -18,9 +56,8 @@ function App() {
           current value between rolls.
         </p>
       </div>
-      <div className="dice-container">
-        <Die value={value} isHeld={isHeld} holdDie={holdDie} />
-      </div>
+      <div className="dice-container">{placeDice}</div>
+      <button onClick={rollDice}>Roll</button>
     </div>
   );
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,8 +1,8 @@
-const Die = ({ value, isHeld, holdDie }) => {
+const Die = ({ value, isHeld, holdDie, id }) => {
   const dieState = isHeld ? "freeze" : "";
 
   return (
-    <button className={"die "+dieState} onClick={() => holdDie()}>
+    <button className={"die "+dieState} onClick={() => holdDie(id)}>
       <strong>{value}</strong>
     </button>
   );
